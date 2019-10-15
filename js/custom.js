376a12d72ee3cb2c3e28ae4c05defcd8b489182c
r-liciotti <35048026+r-liciotti@users.noreply.github.com>
2019-10-15 22:09:46 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,134 @@
+
+
+
+
+
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  var partJson =
+  {
+    "particles": {
+      "number": {
+        "value": 251,
+        "density": {
+          "enable": true,
+          "value_area": 1104.8066982851817
+        }
+      },
+      "color": {
+        "value": "#464646"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 1,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 7.891476416322726,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 4,
+          "size_min": 0.3,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 600
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 479.5204795204795,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 250,
+          "size": 0,
+          "duration": 2,
+          "opacity": 0,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 400,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  };
+
+  var jsonUri = "data:text/plain;base64,"+window.btoa(JSON.stringify(partJson));
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles-js', jsonUri, function() {
+  });
+
+
+  $( "#toPortfolio" ).click(function() {
+$( ".main" ).toggle( "slide", 1400 );
+$(".main").load("portfolio.html");
+
+    });
+
+
+ });
